c4cd623077d79df41eb25cb1bfec5d09aabca641
tricera-steve-beyer <steve.beyer@tricera.energy>
2026-09-22 15:22:34 +0200

    Echoes of engimatus part 1 & 2

--- lib/eni.go
+++ lib/eni.go
@@ -1,22 +1,23 @@
 package lib
 
 import (
-	"slices"
 	"strconv"
+	"strings"
 )
 
-func Eni(n int, exp int, mod int) int {
-	var score int = 1
-	var remainders []int
+const MAX_REMAINDERS = 5
 
-	for range exp {
-		score = score * n
-		remain := score % mod
+func EniVar(n int, exp int, mod int, remainders []int) int {
+	capacity := cap(remainders)
+	if capacity == 0 || exp < MAX_REMAINDERS {
+		capacity = exp
+	}
 
+	for i := 0; i < capacity; i++ {
+		remain := modularExp(n, (exp - i), mod)
 		remainders = append(remainders, remain)
 	}
 
-	slices.Reverse(remainders)
 	val, err := concatInts(remainders)
 
 	if err != nil {
@@ -26,11 +27,41 @@ func Eni(n int, exp int, mod int) int {
 	return val
 }
 
+func EniCycle
+
+func Eni(n int, exp int, mod int) int {
+	var remainders []int
+	return EniVar(n, exp, mod, remainders)
+}
+
+func Eni5(n int, exp int, mod int) int {
+	var remainders [5]int
+	return EniVar(n, exp, mod, remainders[:])
+}
+
+func EniSum(a, b, c, x, y, z, m int) int {
+	return Eni5(a, x, m) + Eni5(b, y, m) + Eni5(c, z, m)
+}
+
 func concatInts(integers []int) (int, error) {
-	s := ""
+	var s strings.Builder
 	for _, n := range integers {
-		s += strconv.Itoa(n)
+		s.WriteString(strconv.Itoa(n))
 	}
 
-	return strconv.Atoi(s)
+	return strconv.Atoi(s.String())
+}
+
+func modularExp(n int, exp int, mod int) int {
+	var score int = 1
+	n = n % mod
+	for exp > 0 {
+		if exp%2 == 1 {
+			score = (score * n) % mod
+		}
+		n = (n * n) % mod
+		exp /= 2
+	}
+
+	return score
 }
